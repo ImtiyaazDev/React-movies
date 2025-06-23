81629efe349af021d38682d4bd31b5b30f15f9d8
ImtiyaazDev <Imtiyaazmac@gmail.com>
2025-06-23 05:35:50 +0200

    feat: Load movies from api

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,54 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Search from "./commponents/Search";
+
+const API_BASE_URL = "https://api.themoviedb.org/3";
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+const API_OPTIONS = {
+	method: "GET",
+	headers: {
+		accept: "application/json",
+		Authorization: `Bearer ${API_KEY}`
+	}
+};
 
 export default function App() {
 	const [searchTerm, setSearchTerm] = useState("");
+	const [errorMessage, setErrorMessage] = useState("");
+	const [movies, setMovies] = useState([]);
+	const [isLoading, setIsLoading] = useState(false);
+
+	const fetchMovies = async function () {
+		setIsLoading(true);
+		setErrorMessage("");
+
+		try {
+			const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+			const res = await fetch(endpoint, API_OPTIONS);
+
+			if (!res.ok) {
+				throw new Error("Failed to fetch movies");
+			}
+
+			const data = await res.json();
+
+			if (data.Response === "False") {
+				setErrorMessage(data.Error || "Failed to fetch movies");
+				setMovies([]);
+				return;
+			}
+
+			setMovies(data.results || []);
+		} catch (err) {
+			console.error(`Error fetch movies: ${err}`);
+			setErrorMessage(`Error fetching movies. Please try again later.`);
+		} finally {
+			setIsLoading(false);
+		}
+	};
+
+	useEffect(function () {
+		fetchMovies();
+	}, []);
 
 	return (
 		<main>
@@ -19,6 +65,26 @@ export default function App() {
 						setSearchTerm={setSearchTerm}
 					/>
 				</header>
+				<section className="all-movies">
+					<h2>All Movies</h2>
+
+					{isLoading ? (
+						<p className="text-white">Loading...</p>
+					) : errorMessage ? (
+						<p className="text-red-500">{errorMessage}</p>
+					) : (
+						<ul>
+							{movies.map((movie, i) => (
+								<p
+									className="text-white"
+									key={i}
+								>
+									{movie.title}
+								</p>
+							))}
+						</ul>
+					)}
+				</section>
 			</div>
 		</main>
 	);
